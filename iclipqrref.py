e42171c22c281092f6048891b578e3b8a552d0ac
Michael Kang <mnkang@eng.ucsd.edu>
2017-02-21 10:11:58 -0800

    Corrected useless url variable and lacking dictionary update to the querystring.

--- iclipqrref.py
+++ iclipqrref.py
@@ -1,30 +1,29 @@
 # coding: utf-8
 """
-iref.py
+iclipref.py
 Date:   2/21/2017
-Usage:  Run in iOS 10 as a Pythonista 3 app extension module with url input from appex.
+Usage:  Run in iOS 10 as a Pythonista 3 app extension module with url input from clipboard.
 Desc:   A simple URL shortener powered by bit.ly for use under Pythonista 3 in iOS 9/10.
-Notes:  Pythonista 3 by OMZ-Software required to use included requests and appex modules on iOS.
+Notes:  Pythonista 3 by OMZ-Software required to use included requests and clipboard.
         Change the access token for analytics purposes.
 """
 import requests
-import appex
 import clipboard
 
+
 def main():
-    if not appex.is_running_extension():
-        print('This script is intended to be run from the sharing extension.')
-        return
+    # if not appex.is_running_extension():
+    #    print('This script is intended to be run from the sharing extension.')
+    #    return
     # My bit.ly access token for https://a.ref.sh/ for demonstration purposes. Change this value for your use.
     ACCESS_TOKEN = "c946a815f11eec030dea06056c146115102c1d1d"
     url = "https://api-ssl.bitly.com/v3/shorten"
     querystring = {"access_token": ACCESS_TOKEN}
 
-    appurl = appex.get_url()
-    if not appurl:
-        print('No input URL found.')
-        return
-    querystring["longUrl"] = appurl
+    text = clipboard.paste()
+    if not text:
+        print('No text in clipboard.')
+    querystring["longUrl"] = text
 
     # HTTP header(s)
     headers = {
@@ -37,12 +36,13 @@ def main():
     # decode as JSON formatted text from requests.request object to dict
     try:
         d = response.json()
+        short = (d['data']).get('url')
         # Print shortened url to console.
-        print((d['data']).get('long_url'))
-        print((d['data']).get('url'))
-
+        clipboard.copy(short)
+        print('Copied ' + short + ' to clipboard.')
     except ValueError:
         print('JSON decoding failed.')
+
 
 if __name__ == '__main__':
     main()
