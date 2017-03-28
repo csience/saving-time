a87598aa31f45dc7c8fa965c4f625356e1c77a42
mnkang <mkanger@me.com>
2017-03-28 00:53:41 -0700

    Socket sniffing.

---
+++ stats.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+
+import socket
+import os
+import sys
+
+host = '192.168.1.119'
+
+if os.name == "nt":
+    socket_protocol = socket.IPPROTO_IP
+else:
+    socket_protocol = socket.IPPROTO_ICMP
+
+sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+
+sniffer.bind((host, 0))
+
+sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+if os.name == "nt":
+    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+print sniffer.recvfrom(65565)
+
+if os.name == "nt":
+    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
